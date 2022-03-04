362918795bf64cf2495ca9c23c1dba4db5df17a8
stefano corso <stefanocorso6@gmail.com>
2022-03-03 22:20:16 -0300

    carga

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container')
+const span = document.getElementById('span')
+let contador = 0 
+
+container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn-info')){
+        contador ++
+        span.textContent = contador
+    } else if ( e.target.classList.contains('btn-danger')){
+        contador --
+        span.textContent = contador
+    }
+    e.stopPropagation()
+} )
